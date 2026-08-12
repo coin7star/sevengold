5be9e37fe4e2f7586384968dcde5571b55dbfdac
coin7star <coin7star@gmail.com>
2026-08-12 14:21:55 +0700

    Add files via upload

--- app/src/main/java/com/sevengold/signalapp/ui/admin/AdminPanelScreen.kt
+++ app/src/main/java/com/sevengold/signalapp/ui/admin/AdminPanelScreen.kt
@@ -38,7 +38,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sevengold.signalapp.data.model.AppUser
